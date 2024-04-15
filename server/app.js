33bbbe93f525e82297648a5a5f709e3d715e00b7
George Manaz <george@MacBook-Air-som-tillhor-George.local>
2024-04-15 16:51:07 +0200

    Login Register authentication

--- server/app.js
+++ server/app.js
@@ -6,11 +6,15 @@ const cors = require("cors")
 app.use(cors())
 const bcrypt = require("bcryptjs");
 
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "sajdakksd12378274983sdhkjsd94r9d99d9d9"
+
 const mongoUrl="mongodb+srv://georgemanazz:familj00@cluster0.c8jcwzx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 mongoose.connect(mongoUrl,{
     useNewUrlParser: true,
-})
+}) 
 .then(() => {
     console.log("Connected to database");
 })
@@ -39,6 +43,40 @@ app.post("/register",async(req, res) => {
     }catch (error) {
         res.send({ status: "error" })
     }
+})
+
+app.post("/login-user", async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email })
+    if (!user) {
+        return res.json({ error: "User Not Found" })
+    }
+    if(await bcrypt.compare(password, user.password)) {
+        const token = jwt.sign({ email: user.email }, JWT_SECRET)
+
+        if(res.status(201)){
+            return res.json({ status: "ok", data: token });
+        }else {
+            return res.json({ status: "error" })
+        }
+    }
+    res.json({ status: "error", error: "Invalid Password" })
+})
+
+app.post("/userData", async (req, res) => {
+    const { token } = req.body;
+    try {
+        const user = jwt.verify(token, JWT_SECRET);
+        console.log(user)
+        const useremail = user.email;
+        User.findOne({ email: useremail }).then((data) => {
+            res.send({ status: "ok", data: data })
+        })
+        .catch((error) => {
+            res.send({ status: "error", data: error })
+        })
+    } catch (error) {}
 })
 
 app.listen(5000,()=>{
